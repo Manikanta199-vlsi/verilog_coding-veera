77c4d07749c898df726037be89cbc58b2d9bd456
Manikanta <manitrade789@gmail.com>
2025-08-17 14:53:17 +0530

    Update 02 - Output Zero.v

--- 1 - Basics/02 - Output Zero.v
+++ 1 - Basics/02 - Output Zero.v
@@ -7,3 +7,6 @@ assign one = 1'b1;
 assign zero = 1'b0;
 
 {3'b111, 3'b000} => 6'b111000
+{1'b1, 1'b0, 3'b101} => 5'b10101
+{4'ha, 4'd10} => 8'b10101010     // 4'ha and 4'd10 are both 4'b1010 in binary
+                                  // 4 ' Hexa A = 10 in Decimal = 1010 in binary
