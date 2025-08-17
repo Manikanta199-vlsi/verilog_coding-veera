18632949cce198eeb8cb63a54223c246bee7f009
Manikanta <manitrade789@gmail.com>
2025-08-17 14:37:55 +0530

    Update 3 - Vector part select.v

--- 2 - Vectors/3 - Vector part select.v
+++ 2 - Vectors/3 - Vector part select.v
@@ -12,3 +12,8 @@ module top_module(
     assign out[23:16] = in[15:8];
     assign out[31:24] = in[7:0];
 endmodule
+===============================================================	
+    assign out[31:24] = in[ 7: 0];	
+	assign out[23:16] = in[15: 8];	
+	assign out[15: 8] = in[23:16];	
+	assign out[ 7: 0] = in[31:24];	
