0736404cc81538a485449bd5250739a04ba1baa1
Manikanta <manitrade789@gmail.com>
2025-08-17 15:21:06 +0530

    Update 6 - Vector Concatanation Operator.v

--- 2 - Vectors/6 - Vector Concatanation Operator.v
+++ 2 - Vectors/6 - Vector Concatanation Operator.v
@@ -13,3 +13,20 @@ module top_module (
     assign z = concat_reg[7:0];
 
 endmodule
+
+=================================================================================
+        assign w [7:3] = a[4:0];
+        assign w[2:0]=b[4:2];
+
+        assign x [7:6] = b[1:0];
+        assign x[5:1]=c[4:0];
+        assign x[0] = d[4];
+
+        assign y [7:4] = d[3:0];
+        assign y[3:0]=e[4:1];
+
+        assign z[7] = e[0];
+        assign z [6:2] = f[4:0];
+
+        assign z[1]=1'b1;
+        assign z[0]=1'b1;
